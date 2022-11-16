2648d23b5f285caf9f52c00c1ce5d1479f2e2d7f
Manohin <111537419+Manohin@users.noreply.github.com>
2022-11-16 04:03:26 +0300

    Initial Commit

---
+++ FIndNumber/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FIndNumber
+//
+//  Created by Alexey Manokhin on 16.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
